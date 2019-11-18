e493e9480137c4819ceec1018070c2b32115784b
Hassan Abu Bakar <odin88@gmail.com>
2019-11-19 04:53:01 +0800

    another update

--- deploy.sh
+++ deploy.sh
@@ -51,22 +51,11 @@ fi
 
 #https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
 echo "Try to install composer"
-EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
-fi
-
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-exit $RESULT
+curl -sS https://getcomposer.org/installer
 echo "Done installing composer"
+
+chmod 777 composer.phar
+mv composer.phar /usr/local/bin/composer
 
 # 5. Install Composer modules 
 if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
